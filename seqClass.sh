54bbb005635e837dc17a79c020323146d9a73cb1
Gerard T <gerardts9@gmail.com>
2020-02-13 18:13:55 +0100

    Piece of code explained

--- seqClass.sh
+++ seqClass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 seq=$1
-seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
+seq=$(echo $seq | tr a-z A-Z)  # Identifies the type of sequence
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
   if [[ $seq =~ T ]]; then
     echo "The sequence is DNA"
@@ -12,7 +12,7 @@ if [[ $seq =~ ^[ACGTU]+$ ]]; then
 else
   echo "The sequence is not DNA nor RNA"
 fi
-motif=$(echo $2 | tr a-z A-Z)
+motif=$(echo $2 | tr a-z A-Z) # Finds simple motifs in the sequence
 if [[ -n $motif ]]; then
   echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
   if [[ $seq =~ $motif ]]; then
